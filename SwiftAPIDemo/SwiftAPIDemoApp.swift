817564f1d6fddff6d7191e61dc14f5253a8b5a91
RaviDwivedi21 <ravi.dwivedi@wipro.com>
2025-08-05 22:07:02 +0530

    Initial Commit

---
+++ SwiftAPIDemo/SwiftAPIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAPIDemoApp.swift
+//  SwiftAPIDemo
+//
+//  Created by Ravi Dwivedi on 05/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAPIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftAPIDemoUITests/SwiftAPIDemoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftAPIDemoUITestsLaunchTests.swift
+//  SwiftAPIDemoUITests
+//
+//  Created by Ravi Dwivedi on 05/08/25.
+//
+
+import XCTest
+
+final class SwiftAPIDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
